b8c76b1178ba64bfe12e197fd0a58234ca721618
Denperidge <cat@denperidge.com>
2024-11-25 14:17:51 +0100

    First image display

--- .eleventy.js
+++ .eleventy.js
@@ -8,33 +8,49 @@ export const config = {
     }
 }
 
+function getConfig(eleventyConfig) {
+    const rawLinks = JSON.parse(readFileSync(
+        "config/links.json", { encoding: "utf8" })
+    );
+    console.log("Read links.json...")
+    const images = globSync("config/*[!json]").map((image) => {
+        let match = image.split(".")[0].toLowerCase();
+        if (match.includes("/")) {
+            match = match.split("/").pop()
+        }
+        return { 
+            match: match,
+            src: image
+        }
+    });
+
+    const links = rawLinks.map(linkObj => {
+        let [title, url] = linkObj;
+        const titleMatch = title.match(/.*?(?= |$)/)[0].toLowerCase()
+
+        const image = images.find((image) => image.match == titleMatch);
+        if (image === undefined) {
+            return {title, url}
+        } else {
+            console.log("defiend")
+            eleventyConfig.addPassthroughCopy(image.src)
+            return {
+                title: title, 
+                url: url,
+                src: image.src,
+                alt: `Logo for ${title}`
+            }
+        }
+
+    })
+
+    console.log("2222")
+    console.log(links)
+    return links;
+}
+
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("node_modules/@picocss/pico/css/pico.yellow.min.css")
 
-    eleventyConfig.addGlobalData("links", () => {
-        const links = JSON.parse(readFileSync(
-            "config/links.json", { encoding: "utf8" })
-        );
-        console.log("Read links.json...")
-        const images = globSync("config/*[!json]").map((image) => {
-            return { 
-                match: image.split(".")[0].toLowerCase(),
-                src: image
-            }
-        });
-
-        links.map(linkObj => {
-            let [title, url] = linkObj;
-            title = title.match(/.*?(?= |$)/)[0].toLowerCase()
-
-            const image = images.find((image) => image.match == title);
-            if (image === undefined) {
-                return [title, url]
-            } else {
-                return [title, url, image.src]
-            }
-
-        })
-        return links;
-    });
+    eleventyConfig.addGlobalData("links", getConfig(eleventyConfig));
 }
